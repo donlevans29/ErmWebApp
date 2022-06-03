e9efb230cc14e135d7a8cc7259a8ad21884e890c
donlevans29 <donlevans29@gmail.com>
2022-06-03 18:32:31 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Project for Ankhkasta Natural Healing
+// Admin dashboard to better work with clients
+// Author: Zahib Ziaweet
+// Company SQUIDUX
+
+
+// Import stylesheets
+import './style.css';
+
+// Main
+const dashboardWrap = document.getElementById('dashboardWrapper');
+
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1> ANH Life Web App </h1>`;
+
+// const signupDiv = document.getElementById('signUp');
+// signupDiv.innerHTML = `
+
+// <div type='label'>Email Address</div>
+// <input type='Email'></input>
+// <button>SignIn </button>
+
+// `;
+
